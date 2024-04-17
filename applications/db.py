4c58d996fc5c1abd5a1a77fd562800fd41ae9334
Bhav Beri <bhavberi@gmail.com>
2024-04-17 14:50:10 +0530

    Updated mongo connection in db.py

--- applications/db.py
+++ applications/db.py
@@ -2,15 +2,16 @@ from os import getenv
 from pymongo import MongoClient
 
 # get mongodb URI and database name from environment variale
-MONGO_URI = "mongodb://{}:{}@mongo:{}/".format(
+MONGO_URI = "mongodb://{}:{}@{}:{}/".format(
     getenv("MONGO_USERNAME", default="username"),
     getenv("MONGO_PASSWORD", default="password"),
-    getenv("MONGO_PORT", default="27017")
+    getenv("MONGO_CONNECTION_NAME", default="localhost"),
+    getenv("MONGO_PORT", default="27017"),
 )
 MONGO_DATABASE = getenv("MONGO_DATABASE", default="mydb")
 
 # instantiate mongo client
-client = MongoClient(MONGO_URI, uuidRepresentation='standard')
+client = MongoClient(MONGO_URI, uuidRepresentation="standard")
 
 # get database
 db = client[MONGO_DATABASE]
