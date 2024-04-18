5e7a016f6e890fb8dcd73dd6063d92aaac99f4c0
Bhav Beri <bhavberi@gmail.com>
2024-04-19 00:43:27 +0530

    Fix the make listing API, get user details functions and the formatting

--- applications/application_router.py
+++ applications/application_router.py
@@ -2,7 +2,15 @@ from fastapi import APIRouter, HTTPException, Depends, Response, Request, status
 import requests
 
 from models.applications import Listing, Report
-from models.applications_otypes import ListingResponse, ApplicationResponse, UserApplication, Approval, Applications, ApplicationInput, Listings
+from models.applications_otypes import (
+    ListingResponse,
+    ApplicationResponse,
+    UserApplication,
+    Approval,
+    Applications,
+    ApplicationInput,
+    Listings,
+)
 from utils import (
     create_listing,
     validate_role_admin,
@@ -17,56 +25,55 @@ from utils import (
     validate_user_application,
     validate_listing,
     get_all_applications,
-    validate_listing_name,
     validate_user_and_application_user,
     get_all_listings,
     get_user,
+    get_user_details,
 )
 
 router = APIRouter()
 
 
 # make listing
-@router.post("/make_listing", status_code=status.HTTP_201_CREATED, response_model=ListingResponse)
+@router.post(
+    "/make_listing", status_code=status.HTTP_201_CREATED, response_model=ListingResponse
+)
 async def make_listing(
     request: Request,
     listing: Listing,
+    user=Depends(get_user),
 ):
-    
-    user = get_user(request)
     print("===============================")
     print(user)
     print("===============================")
-    
+
     # check if user is admin
-    if not validate_role_admin(user.role):
+    if not validate_role_admin(user["role"]):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="No Permission",
             headers={"set-cookie": ""},
         )
-    
+
     # check if listing already exists
-    if validate_listing_name(listing.name):
+    if validate_listing(listing.name):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Listing already exists",
             headers={"set-cookie": ""},
         )
-    
+
     # create listing
     create_listing(listing.model_dump())
 
     return {"name": listing.name}
 
 
-
 # Get all the listings
 @router.get("/get_Listings", status_code=status.HTTP_200_OK, response_model=Listings)
 async def get_listings():
-    
     # check if user if logged in
-    url = '/api/users/details'
+    url = "/api/users/details"
     response = requests.get(url)
     if response.status_code == 200:
         user = response.json()
@@ -76,7 +83,7 @@ async def get_listings():
             detail="Not logged in",
             headers={"set-cookie": ""},
         )
-    
+
     # validate if the user is applicant or recruiter
     if not (validate_role_recruiter(user.role) or validate_role_candidate(user.role)):
         raise HTTPException(
@@ -84,21 +91,21 @@ async def get_listings():
             detail="No Permission",
             headers={"set-cookie": ""},
         )
-    
+
     listings = Listings()
     listings.listings = get_all_listings()
-    return listings   # validate if correct
-
+    return listings  # validate if correct
 
 
 # apply for job
-@router.post("/apply", status_code=status.HTTP_201_CREATED, response_model= ApplicationResponse)
+@router.post(
+    "/apply", status_code=status.HTTP_201_CREATED, response_model=ApplicationResponse
+)
 async def apply(
     application: ApplicationInput,
 ):
-    
     # check if user if logged in
-    url = '/api/users/details'
+    url = "/api/users/details"
     response = requests.get(url)
     if response.status_code == 200:
         user = response.json()
@@ -108,7 +115,7 @@ async def apply(
             detail="Not logged in",
             headers={"set-cookie": ""},
         )
-    
+
     # check if the logged in user is applying for himself
     if application.user != user.username:
         raise HTTPException(
@@ -116,7 +123,7 @@ async def apply(
             detail="Incorrect Username",
             headers={"set-cookie": ""},
         )
-    
+
     # check if the user is candidate
     if not validate_role_candidate(user.role):
         raise HTTPException(
@@ -124,7 +131,7 @@ async def apply(
             detail="No Permission",
             headers={"set-cookie": ""},
         )
-    
+
     # validate if such a listing exists
     if not validate_listing(application.listing):
         raise HTTPException(
@@ -132,7 +139,7 @@ async def apply(
             detail="No such listing",
             headers={"set-cookie": ""},
         )
-    
+
     # validae if the application already exists
     if validate_user_application(application.user, application.listing):
         raise HTTPException(
@@ -140,30 +147,32 @@ async def apply(
             detail="Application already exists",
             headers={"set-cookie": ""},
         )
-    
+
     # validate if the twitter and the linkedin links are correct
-    if not validate_twitter(application.twitter_id) or not validate_linkedin(application.linkedin_id):
+    if not validate_twitter(application.twitter_id) or not validate_linkedin(
+        application.linkedin_id
+    ):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Wrong social Media handles",
             headers={"set-cookie": ""},
         )
-    
+
     # create application
     create_application(application.model_dump())
 
     return {"user": application.user}
 
 
-
 # get all the applications for a particular listing
-@router.get("/get_applications", status_code=status.HTTP_200_OK, response_model=Applications)
+@router.get(
+    "/get_applications", status_code=status.HTTP_200_OK, response_model=Applications
+)
 async def get_applications(
     listing: Listing,
 ):
-    
     # check if user if logged in
-    url = '/api/users/details'
+    url = "/api/users/details"
     response = requests.get(url)
     if response.status_code == 200:
         user = response.json()
@@ -173,7 +182,7 @@ async def get_applications(
             detail="Not logged in",
             headers={"set-cookie": ""},
         )
-    
+
     # validate if the user is recruiter
     if not validate_role_recruiter(user.role):
         raise HTTPException(
@@ -181,7 +190,7 @@ async def get_applications(
             detail="No Permission",
             headers={"set-cookie": ""},
         )
-    
+
     # validate if listing exists
     if not validate_listing(listing.name):
         raise HTTPException(
@@ -189,11 +198,10 @@ async def get_applications(
             detail="No such listing",
             headers={"set-cookie": ""},
         )
-    
+
     applications = Applications()
     applications.applications = get_all_applications(listing.name)
-    return applications   # validate if correct
-
+    return applications  # validate if correct
 
 
 # get the report for an application
@@ -201,9 +209,8 @@ async def get_applications(
 async def get_report(
     userapplication: UserApplication,
 ):
-    
     # check if user if logged in
-    url = '/api/users/details'
+    url = "/api/users/details"
     response = requests.get(url)
     if response.status_code == 200:
         user = response.json()
@@ -213,7 +220,7 @@ async def get_report(
             detail="Not logged in",
             headers={"set-cookie": ""},
         )
-    
+
     # validate if the user is recruiter
     if not validate_role_recruiter(user.role):
         raise HTTPException(
@@ -221,7 +228,7 @@ async def get_report(
             detail="No Permission",
             headers={"set-cookie": ""},
         )
-    
+
     # validate if user exists
     if not validate_user(userapplication.username):
         raise HTTPException(
@@ -229,9 +236,11 @@ async def get_report(
             detail="No such user",
             headers={"set-cookie": ""},
         )
-    
+
     # validate if the application exists
-    application = get_user_application(userapplication.username, userapplication.listing)
+    application = get_user_application(
+        userapplication.username, userapplication.listing
+    )
 
     if not application:
         raise HTTPException(
@@ -239,12 +248,12 @@ async def get_report(
             detail="No application by this user",
             headers={"set-cookie": ""},
         )
-    
-    report = Report()    # can use builder pattern here. For building the report
+
+    report = Report()  # can use builder pattern here. For building the report
 
     report.user = userapplication.username
 
-    url = '/api/AI/llama'
+    url = "/api/AI/llama"
     response = requests.get(url)
     if response.status_code == 200:
         report.llama = response.text
@@ -254,8 +263,8 @@ async def get_report(
             detail="AI didn't respond",
             headers={"set-cookie": ""},
         )
-    
-    url = '/api/AI/mbti'
+
+    url = "/api/AI/mbti"
     response = requests.get(url)
     if response.status_code == 200:
         report.mbti = response.text
@@ -265,8 +274,8 @@ async def get_report(
             detail="AI didn't respond",
             headers={"set-cookie": ""},
         )
-    
-    url = '/api/AI/report_gen'
+
+    url = "/api/AI/report_gen"
     response = requests.get(url)
     if response.status_code == 200:
         report.report_gen = response.text
@@ -276,8 +285,8 @@ async def get_report(
             detail="AI didn't respond",
             headers={"set-cookie": ""},
         )
-    
-    url = '/api/AI/sentiment'
+
+    url = "/api/AI/sentiment"
     response = requests.get(url)
     if response.status_code == 200:
         report.sentiment = response.text
@@ -291,15 +300,13 @@ async def get_report(
     return report
 
 
-
 # approve the application
 @router.put("/approve", status_code=status.HTTP_202_ACCEPTED)
 async def approve(
     userapplication: UserApplication,
 ):
-    
     # check if user if logged in
-    url = '/api/users/details'
+    url = "/api/users/details"
     response = requests.get(url)
     if response.status_code == 200:
         user = response.json()
@@ -309,7 +316,7 @@ async def approve(
             detail="Not logged in",
             headers={"set-cookie": ""},
         )
-    
+
     # validate if the user is recruiter
     if not validate_role_recruiter(user.role):
         raise HTTPException(
@@ -317,7 +324,7 @@ async def approve(
             detail="No Permission",
             headers={"set-cookie": ""},
         )
-    
+
     # validate if user exists
     if not validate_user(userapplication.username):
         raise HTTPException(
@@ -325,9 +332,11 @@ async def approve(
             detail="No such user",
             headers={"set-cookie": ""},
         )
-    
+
     # validate if the application exists
-    application = get_user_application(userapplication.username, userapplication.listing)
+    application = get_user_application(
+        userapplication.username, userapplication.listing
+    )
 
     if not application:
         raise HTTPException(
@@ -335,26 +344,26 @@ async def approve(
             detail="No application by this user",
             headers={"set-cookie": ""},
         )
-    
+
     result = approve_application(userapplication.username, userapplication.listing)
     if not result.modified_count:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Failed to update user details!"
-            )
+            detail="Failed to update user details!",
+        )
 
     return {"message": "User application updated successfully!"}
 
 
-
 # chec if the application is approved
-@router.get("/get_application_status", status_code=status.HTTP_200_OK, response_model=Approval)
+@router.get(
+    "/get_application_status", status_code=status.HTTP_200_OK, response_model=Approval
+)
 def get_application_status(
     userapplication: UserApplication,
 ):
-    
     # check if user if logged in
-    url = '/api/users/details'
+    url = "/api/users/details"
     response = requests.get(url)
     if response.status_code == 200:
         user = response.json()
@@ -364,7 +373,7 @@ def get_application_status(
             detail="Not logged in",
             headers={"set-cookie": ""},
         )
-    
+
     # valuidate if the user is a candidate
     if not validate_role_candidate(user.role):
         raise HTTPException(
@@ -372,7 +381,7 @@ def get_application_status(
             detail="No Permission",
             headers={"set-cookie": ""},
         )
-    
+
     # validate if the user os checking his own application status
     if not validate_user_and_application_user(userapplication.username, user):
         raise HTTPException(
@@ -380,9 +389,11 @@ def get_application_status(
             detail="Can't check for other users",
             headers={"set-cookie": ""},
         )
-    
+
     # validate if the application exists
-    application = get_user_application(userapplication.username, userapplication.listing)
+    application = get_user_application(
+        userapplication.username, userapplication.listing
+    )
 
     if not application:
         raise HTTPException(
@@ -390,5 +401,5 @@ def get_application_status(
             detail="No such application",
             headers={"set-cookie": ""},
         )
-    
+
     return {"status": application.accepted}
--- applications/utils.py
+++ applications/utils.py
@@ -1,52 +1,56 @@
-from fastapi import HTTPException, Request, status, FastAPI
+from fastapi import HTTPException, Cookie, status
 import re
 import requests
 
 from models.applications_otypes import ApplicationInput
 from db import db
 
+
 def validate_role_admin(role: str):
     if role != "admin":
         return False
     return True
+
 
 def validate_role_candidate(role: str):
     if role != "candidate":
         return False
     return True
 
+
 def validate_role_recruiter(role: str):
     if role != "recruiter":
         return False
     return True
 
+
 def validate_twitter(id: str):
-    pattern = r'^https?://twitter.com/[A-Za-z0-9_]{1,15}(?:/)?$'
+    pattern = r"^https?://twitter.com/[A-Za-z0-9_]{1,15}(?:/)?$"
     return bool(re.match(pattern, id))
+
 
 def validate_linkedin(id: str):
-    pattern = r'^https?://(?:www\.)?linkedin\.com/in/[A-Za-z0-9_-]+/?$'
+    pattern = r"^https?://(?:www\.)?linkedin\.com/in/[A-Za-z0-9_-]+/?$"
     return bool(re.match(pattern, id))
 
-def validate_user(username: str):   # implement
+
+def validate_user(username: str):  # implement
     return True
+
 
 def validate_user_and_application_user(username_application: str, username: str):
     return username == username_application
 
+
 def validate_listing(name: str):
+    print(name)
     listing = db.listings.find_one({"name": name})
+    print(listing)
     if listing:
         return True
     else:
         return False
 
-def validate_listing_name(name: str):
-    listing = db.listings.find_one({"name": name})
-    if listing:
-        return False
-    else:
-        return True
 
 def validate_user_application(username: str, listingname: str):
     application = db.applications.find_one({"user": username, "listing": listingname})
@@ -54,54 +58,59 @@ def validate_user_application(username: str, listingname: str):
         return False
     return True
 
+
 def create_listing(listing: dict):
     result = db.listings.insert_one(listing)
     return str(result.inserted_id)
+
 
 def create_application(application: ApplicationInput):
     application["approved"] = False
     result = db.applications.insert_one(application)
     return str(result.inserted_id)
 
+
 def get_user_application(username: str, listing: str):
     application = db.applications.find_one({"user": username, "listing": listing})
     return application
+
 
 def get_all_applications(name: str):
     applications = db.applications.find({"listing": name})
     return applications
 
+
 def get_all_listings():
     listings = db.listings.find()
     return listings
 
+
 def approve_application(username: str, listing: str):
-    result = db.applications.update_one({"username": username, "listing": listing}, {
-                        "$set": {"accepted": True}})
+    result = db.applications.update_one(
+        {"username": username, "listing": listing}, {"$set": {"accepted": True}}
+    )
     return result
 
-def get_user(request: Request):
-    username = request.session.get('username')
-    session = requests.Session()
-    session.headers['username'] = username
 
-    print("here")
-    url = 'http://users/details'
-    response = session.get(url)
-    print(response)
+def get_user(access_token_se_p3: str = Cookie(None)):
+    url = "http://users/current"
+    response = requests.get(url, cookies={"access_token_se_p3": access_token_se_p3})
     if response.status_code == 200:
-        print("got_here")
         return response.json()
     else:
         raise HTTPException(
-            status_code=400,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail="Not logged in",
-            headers={"set-cookie": ""},
         )
-    
-# app = FastAPI()
-# @app.get('/get_firstuser')
-# def get_user():
-#     api_url = "http://localhost/api/users/details"
-#     user = requests.get(api_url).json()
-#     return user
+
+
+def get_user_details(access_token_se_p3: str = Cookie(None)):
+    url = "http://users/details"
+    response = requests.get(url, cookies={"access_token_se_p3": access_token_se_p3})
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Not logged in",
+        )
