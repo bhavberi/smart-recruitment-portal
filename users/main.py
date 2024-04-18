8a687f5202c3c137517d6f7d82c0cae2bc7efe64
Bhav Beri <bhavberi@gmail.com>
2024-04-19 00:41:23 +0530

    Moved from Session Based to Cookies Based Authentication for ease and better use

--- users/main.py
+++ users/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-from starlette.middleware.sessions import SessionMiddleware
 from os import getenv
 import users_router
 
 DEBUG = getenv("BACKEND_DEBUG", "False").lower() in ("true", "1", "t")
-SESSION_SECRET_KEY = getenv("SESSION_SECRET_KEY", "this_is_my_very_secretive_secret")
 
 # Create a new FastAPI instance
 # FastAPI App
@@ -29,9 +27,6 @@ else:
 @app.get("/ping", tags=["General"])
 async def index():
     return {"message": "Backend Running!!"}
-
-# Add Session Middleware
-app.add_middleware(SessionMiddleware, secret_key=SESSION_SECRET_KEY)
 
 # Mount the user router on the "/user" path
 app.include_router(users_router.router, tags=["User"])
