f6b47a06314480e48cca33f34234d8fbfad48d10
DivijD012 <divijd111@gmail.com>
2024-04-24 23:35:56 +0530

    Fixing ExistingApplicationValidator

--- applications/validation.py
+++ applications/validation.py
@@ -220,7 +220,7 @@ class ExistingApplicationValidator(AbstractHandler):
         return False
     
     def handle_request(self, request):
-        if self.validate_user_application(request["user"], request["listing"]):
+        if not self.validate_user_application(request["user"], request["listing"]):
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Application already exists",
