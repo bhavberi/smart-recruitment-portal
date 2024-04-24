93ef3a94b6e68d4ba18cbbdf05b420fbac599d20
Bhav Beri <bhavberi@gmail.com>
2024-04-24 23:15:48 +0530

    Updated Linkedin Separate Code

--- Separate Services/Linkedin_Service/README.md
+++ Separate Services/Linkedin_Service/README.md
@@ -8,8 +8,20 @@ source .venv/bin/activate
 pip install -r requirements.txt
 ```
 
-After installing the requirements, you can run the service by running the following command:
+Now, you need to set the environment variables. You can do this by copying the `.env.example` file to `.env` and filling in the required values.
+
+```bash
+cp .env.example .env
+```
+
+Then, set the LINKEDIN Crednetials in the env file.
+
+After installing the requirements and setting the environment variables, you can run the service by running the following command:
 
 ```bash
 python3 app.py
 ```
+
+- Now, you can check the service by sending a request to `http://localhost:8080/linkedin/<linkedin_url>`.
+
+    An example of it would be `http://localhost:8080/skills/http://linkedin.com/in/bhavberi`.
--- Separate Services/Linkedin_Service/app.py
+++ Separate Services/Linkedin_Service/app.py
@@ -5,29 +5,35 @@ import regex as re
 
 DEBUG = getenv("DEBUG", "False").lower() in ("true", "1", "t")
 SECRET_KEY = getenv("SECRET_KEY", "secret-key")
-PORT = getenv("PORT", 80)
+PORT = int(getenv("PORT", 8080))
 OUTPUT_FILENAME = getenv("OUTPUT_FILENAME", "output.txt")
 linkedin_regex = "http(s)?://([w]+.)?linkedin.com/(?:company/|in/)[A-z0-9_-]+/?"
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
+
 @app.route("/")
 def index():
     return "Backend Service for Linkedin"
+
 
 @app.route("/ping")
 def ping():
     return {"message": "Backend Running!!"}
 
-@app.route('/skills')
-def skills(linkedin_link: str):
+
+@app.route("/skills", defaults={"linkedin_link": None})
+@app.route("/skills/<path:linkedin_link>")
+def skills(linkedin_link):
     if not linkedin_link:
         return {"message": "Please provide a linkedin profile link"}
     if not re.match(linkedin_regex, linkedin_link):
         return {"message": "Invalid Linkedin Profile Link"}
-    
-    system(f"python3 scraper_linkedin/linkedin.py --profile_link={linkedin_link} > {OUTPUT_FILENAME}")
+
+    system(
+        f"python3 scraper_linkedin/linkedin.py --profile_link={linkedin_link} > {OUTPUT_FILENAME}"
+    )
     fp = open(OUTPUT_FILENAME, "r")
     jobs = fp.read()
     fp.close()
@@ -35,8 +41,9 @@ def skills(linkedin_link: str):
 
     jobs_index = jobs.find("\n{")
     jobs = jobs[jobs_index:]
-    cleaned_string = jobs.replace("\n", "").replace("'", "\"")
+    cleaned_string = jobs.replace("\n", "").replace("'", '"')
     return json.loads(cleaned_string)
 
+
 if __name__ == "__main__":
-    app.run(debug=DEBUG, host="0.0.0.0", port=80)
+    app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
