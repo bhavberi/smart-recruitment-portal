bf809d9fa04a45feaaf18d1ce871ac02fbe88660
Bhav Beri <bhavberi@gmail.com>
2024-05-29 16:15:23 +0530

    Minor changes left on local

--- AI/llama/app.py
+++ AI/llama/app.py
@@ -22,15 +22,15 @@ def llama_call(mbti):
         + ". Dont refer to yourself or the promter. Also dont give an introduction or greeting."
     )
     system_promt = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. Also, try to give responses which are impersonal, meaning try not to refer to yourself or the asker.If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
-    # output = replicate.run(
-    #     "meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3",
-    #     input={
-    #         "prompt": promt,
-    #         "max_new_tokens": 500,
-    #         "system_promt": system_promt,
-    #     },
-    # )
-    output = ["Hello World", "Bye World", "New World", "Old World"]
+    output = replicate.run(
+        "meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3",
+        input={
+            "prompt": promt,
+            "max_new_tokens": 500,
+            "system_promt": system_promt,
+        },
+    )
+    # output = ["Hello World", "Bye World", "New World", "Old World"]
     # The meta/llama-2-70b-chat model can stream output as it's running.
     # The predict method returns an iterator, and you can iterate over that output.
 
